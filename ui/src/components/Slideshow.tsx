78069905775f21856bcb7c5ea4f1f96cc3961600
Kálóczi Kevin <kaloczi.kvn@gmail.com>
2024-03-02 17:07:10 +0100

    vite and more
    - added vite insted of CRA
    - updated packages
    - use screen height insted of width
    - custom game mode support with regex
    - removed unnecessary code and packages

--- ui/src/components/Slideshow.tsx
+++ ui/src/components/Slideshow.tsx
@@ -1,67 +1,61 @@
-import React from 'react';
-import { Fade } from 'react-slideshow-image';
+import React from "react";
+import { Fade } from "react-slideshow-image";
 
-interface Props {
-    map: string|null;
-    customMapName: string|null;
-    customMapImages: any;
+interface IProps {
+  mapName: string | null;
+  customMapName: string | null;
+  customMapImages: any;
 }
 
-const Slideshow: React.FC<Props> = ({
-    map,
-    customMapName,
-    customMapImages,
-}) => {
-    const host = "https://cdn.jsdelivr.net/gh/kaloczikvn/VU-Loading-Images@master/";
-    let list = ["01", "02", "03", "04"];
+const HOST = "https://cdn.jsdelivr.net/gh/kaloczikvn/VU-Loading-Images@master";
 
-    return (
-        <>
-            {map &&
-                <Fade arrows={false} pauseOnHover={false} canSwipe={false} autoplay={true} defaultIndex={(Math.floor(Math.random() * 3))}>
-                    {(customMapImages !== null && (customMapImages[map] !== undefined || customMapImages[customMapName!] !== undefined)) ?
-                        (customMapName !== null ?
-                            customMapImages[customMapName].sort(() => Math.random() - 0.5).map((pic: string, index: number) => (
-                                <div className="slideItem" key={index}>
-                                    <img 
-                                        src={pic} 
-                                        alt="slideItem0" 
-                                        onError={(e: any) => {e.target.onerror = null; e.target.src = "images/default.jpg"}} 
-                                        onLoad={(e: any) => {e.target.style = {opacity: 1}}} 
-                                        style={{opacity: 0}} 
-                                    />
-                                </div>
-                            ))
-                        :
-                            customMapImages[map].sort(() => Math.random() - 0.5).map((pic: string, index: number) => (
-                                <div className="slideItem" key={index}>
-                                    <img 
-                                        src={pic} 
-                                        alt="slideItem0" 
-                                        onError={(e: any) => {e.target.onerror = null; e.target.src = "images/default.jpg"}} 
-                                        onLoad={(e: any) => {e.target.style = {opacity: 1}}} 
-                                        style={{opacity: 0}} 
-                                    />
-                                </div>
-                            ))
-                        )
-                    :
-                        list.sort(() => Math.random() - 0.5).map((pic: string, index: number) => (
-                            <div className="slideItem" key={index}>
-                                <img 
-                                    src={host + map + "/" + pic + ".jpg"} 
-                                    alt="slideItem0" 
-                                    onError={(e: any) => {e.target.onerror = null; e.target.src = "images/default.jpg"}} 
-                                    onLoad={(e: any) => {e.target.style = {opacity: 1}}} 
-                                    style={{opacity: 0}} 
-                                />
-                            </div>
-                        ))
-                    }
-                </Fade>
-            }
-        </>
-    )
+const Slideshow: React.FC<IProps> = ({ mapName, customMapName, customMapImages }) => {
+  if (!mapName) {
+    return <></>;
+  }
+
+  const getImages = () => {
+    if (customMapImages !== null) {
+      if (customMapName && customMapImages[customMapName]) {
+        return customMapImages[customMapName];
+      }
+
+      if (customMapImages[mapName]) {
+        return customMapImages[mapName];
+      }
+    }
+
+    const _tempImages = [];
+    for (let i = 1; i <= 4; i++) {
+      _tempImages.push(`${HOST}/${mapName}/0${i}.jpg`);
+    }
+    return _tempImages;
+  };
+
+  const images = getImages();
+
+  return (
+    <Fade arrows={false} pauseOnHover={false} canSwipe={false} autoplay={true} defaultIndex={Math.floor(Math.random() * 3)}>
+      {images
+        .sort(() => Math.random() - 0.5)
+        .map((pic: string, index: number) => (
+          <div className="slideItem" key={`${mapName}-${index}`}>
+            <img
+              src={pic}
+              alt=""
+              onError={(e: any) => {
+                e.target.onerror = null;
+                e.target.src = "images/default.jpg";
+              }}
+              onLoad={(e: any) => {
+                e.target.style = { opacity: 1 };
+              }}
+              style={{ opacity: 0 }}
+            />
+          </div>
+        ))}
+    </Fade>
+  );
 };
 
 export default Slideshow;
